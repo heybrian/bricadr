1f99b8394e3e0076d1519f304a2814bd3ab8069f
Brian Johnson <heybej@gmail.com>
2023-06-01 14:22:17 -0700

    Mouseover arrows and underline added to StockTable column headers

--- src/StockTable.js
+++ src/StockTable.js
@@ -8,7 +8,8 @@ class StockTable extends React.Component {
       originalStocks: props.stocks,
       stocks: props.stocks,
       sortColumn: 'name',
-      sortAscending: true
+      sortAscending: true,
+      hoverColumn: null  // add this line
     };
   }
 
@@ -67,41 +68,104 @@ class StockTable extends React.Component {
     return (
       <div className='container stock-table'>
         <div className='row bg-primary text-white'>
-          <div className='col-3 col-sm-2 text-nowrap' onClick={() => this.sort('name')}>
-            {'ADR Name '}
-            {this.state.sortColumn === 'name' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
-          </div>
-          <div className='col-3 col-sm-1 text-nowrap' onClick={() => this.sort('ticker')}>
-            {'Ticker '}
-            {this.state.sortColumn === 'ticker' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
-          </div>
-          <div className='col-2 col-sm-1 text-nowrap' onClick={() => this.sort('price')}>
-            {'Price '}
-            {this.state.sortColumn === 'price' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
-          </div>
-          <div className='col-2 col-sm-1 text-nowrap' onClick={() => this.sort('change')}>
-            {'Change '}
-            {this.state.sortColumn === 'change' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
-          </div>
-          <div className='col-2 col-sm-1 text-nowrap' onClick={() => this.sort('changePercent')}>
-      {'\u00A0\u00A0\u00A0\u00A0\u00A0% '}
+          {/* 'ADR Name' Column */}
+    <div 
+      className={`col-3 col-sm-2 text-nowrap sortable-column ${this.state.sortColumn === 'name' ? 'sort-column' : ''}`} 
+      onClick={() => this.sort('name')}
+      onMouseEnter={() => this.setState({ hoverColumn: 'name' })}
+      onMouseLeave={() => this.setState({ hoverColumn: null })}
+    >
+      <span className="sortable-column-text">{'ADR Name'}</span>
+      {this.state.sortColumn === 'name' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
+      {this.state.hoverColumn === 'name' && this.state.sortColumn !== 'name' && '\u2193'}
+    </div>
+    {/* 'Ticker' Column */}
+    <div 
+      className={`col-3 col-sm-1 text-nowrap sortable-column ${this.state.sortColumn === 'ticker' ? 'sort-column' : ''}`} 
+      onClick={() => this.sort('ticker')}
+      onMouseEnter={() => this.setState({ hoverColumn: 'ticker' })}
+      onMouseLeave={() => this.setState({ hoverColumn: null })}
+    >
+      <span className="sortable-column-text">{'Ticker'}</span>
+      {this.state.sortColumn === 'ticker' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
+      {this.state.hoverColumn === 'ticker' && this.state.sortColumn !== 'ticker' && '\u2193'}
+    </div>
+    {/* 'Price' Column */}
+    <div 
+      className={`col-2 col-sm-1 text-nowrap sortable-column ${this.state.sortColumn === 'price' ? 'sort-column' : ''}`} 
+      onClick={() => this.sort('price')}
+      onMouseEnter={() => this.setState({ hoverColumn: 'price' })}
+      onMouseLeave={() => this.setState({ hoverColumn: null })}
+    >
+      <span className="sortable-column-text">{'Price'}</span>
+      {this.state.sortColumn === 'price' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
+      {this.state.hoverColumn === 'price' && this.state.sortColumn !== 'price' && '\u2193'}
+    </div>
+    {/* 'Change' Column */}
+    <div 
+      className={`col-2 col-sm-1 text-nowrap sortable-column ${this.state.sortColumn === 'change' ? 'sort-column' : ''}`} 
+      onClick={() => this.sort('change')}
+      onMouseEnter={() => this.setState({ hoverColumn: 'change' })}
+      onMouseLeave={() => this.setState({ hoverColumn: null })}
+    >
+      <span className="sortable-column-text">{'Change'}</span>
+      {this.state.sortColumn === 'change' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
+      {this.state.hoverColumn === 'change' && this.state.sortColumn !== 'change' && '\u2193'}
+    </div>
+    {/* 'Change Percent' Column */}
+    <div 
+      className={`col-2 col-sm-1 text-nowrap sortable-column ${this.state.sortColumn === 'changePercent' ? 'sort-column' : ''}`} 
+      onClick={() => this.sort('changePercent')}
+      onMouseEnter={() => this.setState({ hoverColumn: 'changePercent' })}
+      onMouseLeave={() => this.setState({ hoverColumn: null })}
+    >
+      {'\u00A0\u00A0\u00A0'}<span className="sortable-column-text">{'%'}</span>
       {this.state.sortColumn === 'changePercent' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
+      {this.state.hoverColumn === 'changePercent' && this.state.sortColumn !== 'changePercent' && '\u2193'}
     </div>
-    <div className='d-none d-sm-block col-sm-1 text-nowrap' onClick={() => this.sort('marcap')}>
-      {'MarCap '}
+    {/* 'MarCap' Column */}
+    <div 
+      className={`d-none d-sm-block col-sm-1 text-nowrap sortable-column ${this.state.sortColumn === 'marcap' ? 'sort-column' : ''}`} 
+      onClick={() => this.sort('marcap')}
+      onMouseEnter={() => this.setState({ hoverColumn: 'marcap' })}
+      onMouseLeave={() => this.setState({ hoverColumn: null })}
+    >
+      <span className="sortable-column-text">{'MarCap'}</span>
       {this.state.sortColumn === 'marcap' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
+      {this.state.hoverColumn === 'marcap' && this.state.sortColumn !== 'marcap' && '\u2193'}
     </div>
-    <div className='d-none d-sm-block col-sm-1 text-nowrap' onClick={() => this.sort('yield')}>
-      {'Yield '}
+    {/* 'Yield' Column */}
+    <div 
+      className={`d-none d-sm-block col-sm-1 text-nowrap sortable-column ${this.state.sortColumn === 'yield' ? 'sort-column' : ''}`} 
+      onClick={() => this.sort('yield')}
+      onMouseEnter={() => this.setState({ hoverColumn: 'yield' })}
+      onMouseLeave={() => this.setState({ hoverColumn: null })}
+    >
+      <span className="sortable-column-text">{'Yield'}</span>
       {this.state.sortColumn === 'yield' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
+      {this.state.hoverColumn === 'yield' && this.state.sortColumn !== 'yield' && '\u2193'}
     </div>
-    <div className='d-none d-sm-block col-sm-2' onClick={() => this.sort('sector')}>
-      {'Sector '}
+    {/* 'Sector' Column */}
+    <div 
+      className={`d-none d-sm-block col-sm-2 text-nowrap sortable-column ${this.state.sortColumn === 'sector' ? 'sort-column' : ''}`} 
+      onClick={() => this.sort('sector')}
+      onMouseEnter={() => this.setState({ hoverColumn: 'sector' })}
+      onMouseLeave={() => this.setState({ hoverColumn: null })}
+    >
+      <span className="sortable-column-text">{'Sector'}</span>
       {this.state.sortColumn === 'sector' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
+      {this.state.hoverColumn === 'sector' && this.state.sortColumn !== 'sector' && '\u2193'}
     </div>
-    <div className='d-none d-sm-block col-sm-2' onClick={() => this.sort('industry')}>
-      {'Industry '}
+    {/* 'Industry' Column */}
+    <div 
+      className={`d-none d-sm-block col-sm-2 text-nowrap sortable-column ${this.state.sortColumn === 'industry' ? 'sort-column' : ''}`} 
+      onClick={() => this.sort('industry')}
+      onMouseEnter={() => this.setState({ hoverColumn: 'industry' })}
+      onMouseLeave={() => this.setState({ hoverColumn: null })}
+    >
+      <span className="sortable-column-text">{'Industry'}</span>
       {this.state.sortColumn === 'industry' ? (this.state.sortAscending ? '\u2191' : '\u2193') : ''}
+      {this.state.hoverColumn === 'industry' && this.state.sortColumn !== 'industry' && '\u2193'}
     </div>
         </div>
         {this.state.stocks.map(stock => (
